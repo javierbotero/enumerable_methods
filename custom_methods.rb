65c079ebb015966b6b2d142747fcaaf42a7fe4a4
Javier Botero <javierbotero10271@gmail.com>
2020-03-13 13:07:02 -0500

    Add the my_map for Proc or Block given implementation

--- custom_methods.rb
+++ custom_methods.rb
@@ -3,42 +3,50 @@
 module Enumerable 
 
     def my_each 
+      my_array = self
+      my_array = * my_array if my_array.class == Range
       return to_enum if !block_given?
       i = 0
-      while i < length
-        yield self[i]
+      while i < my_array.length
+        yield my_array[i]
         i += 1
       end
-    self 
+    my_array 
     end
 
     def my_each_with_index
+      my_array = self
+      my_array = * my_array if my_array.class == Range
       return to_enum if !block_given?
       i = 0
-      while i < length
-        yield self[i], i
+      while i < my_array.length
+        yield my_array[i], i
         i += 1
       end  
-      self
+      my_array
     end
 
     def my_select
+      my_array = self
+      my_array = * my_array if my_array.class == Range
       return to_enum if !block_given?
       arr = []
       i = 0
-      while i < length 
-        arr.push(self[i]) unless !yield self[i]
+      while i < my_array.length 
+        arr.push(my_array[i]) unless !yield my_array[i]
         i += 1
       end
       arr
     end
 
     def my_all?(pattern = false)
+      my_array = self
+      my_array = * my_array if my_array.class == Range
       if block_given?        
         i = 0
         bool = true
-        while i < length
-          if !yield self[i]
+        while i < my_array.length
+          if !yield my_array[i]
             bool = false 
             break
           end
@@ -47,8 +55,8 @@ module Enumerable
         return bool
       elsif pattern
         i = 0
-        while i < length
-          return false unless pattern === self[i]
+        while i < my_array.length
+          return false unless pattern === my_array[i]
           i += 1
         end
         return true
@@ -58,17 +66,19 @@ module Enumerable
     end
 
     def my_any? (pattern = false)
+      my_array = self
+      my_array = * my_array if my_array.class == Range
       if block_given?
         i = 0
-        while i < length
-          return true if yield self[i] 
+        while i < my_array.length
+          return true if yield my_array[i] 
           i += 1
         end
         return false
       elsif pattern
         i = 0
-        while i < length 
-          return true if pattern === self[i]
+        while i < my_array.length 
+          return true if pattern === my_array[i]
           i += 1
         end
         return false
@@ -78,17 +88,19 @@ module Enumerable
     end
 
     def my_none? (pattern = false)
+      my_array = self
+      my_array = * my_array if my_array.class == Range
       if block_given?
         i = 0
-        while i < length
-          return false if yield self[i] 
+        while i < my_array.length
+          return false if yield my_array[i] 
           i += 1
         end
         return true
       elsif pattern
         i = 0
-        while i < length
-          return false if pattern === self[i]
+        while i < my_array.length
+          return false if pattern === my_array[i]
           i += 1  
         end
         return true
@@ -98,18 +110,20 @@ module Enumerable
     end
 
     def my_count(item=false)
+      my_array = self
+      my_array = * my_array if my_array.class == Range
       counter = 0
       if block_given?       
          i = 0
-         while i < length
-           counter += 1 unless !yield self[i]  
+         while i < my_array.length
+           counter += 1 unless !yield my_array[i]  
            i += 1    
          end
          return counter
       elsif item
         i = 0
-        while i < length
-          counter += 1 unless item != self[i]
+        while i < my_array.length
+          counter += 1 unless item != my_array[i]
           i += 1
         end
         return counter
@@ -118,47 +132,68 @@ module Enumerable
       end  
     end
 
-    def my_map
+    def my_map (proc = false)
+      my_array = self
+      my_array = * my_array if my_array.class == Range
       arr = []
-      if block_given?
+      if block_given? 
         i = 0
-        while i < length
-          arr.push(yield self[i])
+        while i < my_array.length
+          arr.push(yield my_array[i])
           i += 1  
         end 
         return arr
-      end 
-      return to_enum 
+      elsif proc   
+        i = 0
+        while i < my_array.length
+          arr.push(proc.call(my_array[i]))
+          i += 1
+        end
+        return arr
+      else     
+        return to_enum 
+      end
     end
 
-    def my_inject (memo = false, symbol = false)      
+    def my_inject (memo = false, symbol = false)
+      arr = self 
+      if arr.class == Range
+        arr = *arr
+      end  
       if memo && symbol
         i = 0
-        while i < length
-          memo = memo.send(symbol, self[i])  
+        while i < arr.length
+          memo = memo.send(symbol, arr[i])  
           i += 1 
         end
         return memo
       elsif memo && block_given?
         i = 0
-        while i < length
-          memo = yield memo, self[i] 
+        while i < arr.length
+          memo = yield memo, arr[i] 
           i += 1  
         end
         return memo
-      elsif memo    
-        sum = self[0]
+      elsif memo.class == Symbol 
+        sum = arr[0]
         i = 1 
-        while i < length
-          sum = sum.send(memo, self[i])
+        while i < arr.length
+          sum = sum.send(memo, arr[i])
           i += 1
         end
         return sum
-      else
-        memo = self[0]
+      elsif memo.class == Integer
+        i = 0
+        while i < arr.length
+          memo += arr[i]
+          i += 1
+        end
+        return memo
+      else 
+        memo = arr[0]
         i = 1
-        while i < length
-          memo = yield memo, self[i] 
+        while i < arr.length
+          memo = yield memo, arr[i] 
           i += 1
         end
         return memo
@@ -166,4 +201,11 @@ module Enumerable
     end
 
 end
-  
+
+arr = [1 , 2, 3, 4]
+
+def multiply_els (els)
+  els.my_inject(:*)
+end
+
+puts multiply_els(arr)
